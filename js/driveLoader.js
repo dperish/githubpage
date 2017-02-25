5c0c42a6b98d9fb1b21b648c0ee4a0541315d49b
David Lee Perish <davidperish@outlook.com>
2017-02-25 02:08:39 -0500

    Create driveLoader.js

---
+++ js/driveLoader.js
@@ -0,0 +1,40 @@
+briefcase.getJSON({
+    id: "1dBgx1A9_gA_3c_rKC5IpsvLj6ubQY_1Q0Ee51QPOZ-4",
+    leftColumnTitle: "category"
+}, printElements);
+
+function printElements(data) {
+
+    data.forEach(function (item) {
+
+        var category = item.title;
+        var title = item.categories.filter(function (x) { if (x.name == "title") { return x; } })[0].value;
+        var url = item.categories.filter(function (x) { if (x.name == "url") { return x; } })[0].value;
+        var color = item.categories.filter(function (x) { if (x.name == "color") { return x; } })[0].value;
+        var image = item.categories.filter(function(x) { if (x.name == "image") { return x; } })[0].value;
+        
+        switch (category) {
+            case "TopHeadline":
+                $("<a href='" + url + "' style='font-family: monospace; color:" + color + "' target='_blank'>" + 
+                  ((image) ? "<img src='" + image + "' style='max-width: 320px;'><br>" : "")    +               
+                  "    <strong>" + title + "</strong></a><br>")
+                .appendTo("#" + category);
+                break;
+            case "MainHeadline":
+                $(((image) ? "<img src='" + image + "'><br>" : "") +
+                  "  <a href='" + url + "' style='font-size: 3em; text-decoration: underline; font-family: sans-serif; color:" + color + "' target='_blank'>" + 
+                  "    <strong>" + title + "</strong></a>")
+                .appendTo("#" + category);
+                break;
+            default:
+                $("<li class='list-group-item' style='border-left: none; border-right: none; padding: 4px;'>" + 
+                  ((image) ? "<img src='" + image + "' style='max-width: 320px;'><br>" : "") +
+                  "  <a href='" + url + "' style='font-family: monospace; color:" + color + "' target='_blank'>" + 
+                  "    <strong>" + title + "</strong></a></li>")
+                .appendTo("#" + category);
+                break;
+        }
+
+    });
+
+}
