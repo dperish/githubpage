d526cffd895248712c89b45a93b4f80f7ffaa025
David Lee Perish <davidperish@outlook.com>
2017-04-16 11:05:49 -0400

    eg drop

--- js/eggdrop.js
+++ js/eggdrop.js
@@ -2,17 +2,35 @@ $(document).ready(function() {
 
     var positions = JSON.parse(localStorage.getItem("positions")) || [];
 
-    $("#dropButton").on("click", function () {
-
-        navigator.geolocation.getCurrentPosition(function(pos){
-            positions.push(pos);
-            
-            $("<li class='list-group-item'>" 
-                + pos.coords.latitude + ", " + pos.coords.longitude 
-                + "</li>").appendTo("#positionsList");
-
-        });
-
+    $("#clearButton").on("click", function () {
+        localStorage.clear();
     });
+
+    $("#loadButton").on("click", function() {
+        for(var i = 0; i < positions.length; i++) {
+            renderPosition(positions[i]);
+        }
+    })
+
+    $("#dropButton").on("click", function () {
+        navigator.geolocation.getCurrentPosition(function(pos) {
+
+            var position = {
+                "latitude": pos.coords.latitude,
+                "longitude": pos.coords.longitude,
+                "timeStamp": pos.timestamp
+            };
+
+            positions.push(position);
+            localStorage.setItem("positions", JSON.stringify(positions));
+            renderPosition(position);
+        });
+    });
+
+    function renderPosition(position) {
+        $("<li class='list-group-item'>" 
+            + position.latitude + ", " + position.longitude 
+            + "</li>").appendTo("#positionsList");
+    }
 
 });
